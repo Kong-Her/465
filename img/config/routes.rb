4c862c63a667c6ace049030c7f682ede48985b45
Kong Her <kher12@mail.csuchico.edu>
2014-11-13 17:57:43 +0000

    public/private radio button working

--- img/config/routes.rb
+++ img/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :images
+ 
+  root 'images#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
