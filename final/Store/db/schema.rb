72b9540484af079ee2c612effdbcf058397e830c
Kong Her <kher12@mail.csuchico.edu>
2014-12-15 04:20:35 +0000

    Added category model and referenced it to Products.

--- final/Store/db/schema.rb
+++ final/Store/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215020259) do
+ActiveRecord::Schema.define(version: 20141215041758) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -46,12 +46,21 @@ ActiveRecord::Schema.define(version: 20141215020259) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: true do |t|
     t.string   "name"
     t.decimal  "price"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
 end
