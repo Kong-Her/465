c6a68597a30857017c063e972528d4b6e70882ce
Kong Her <kher12@mail.csuchico.edu>
2014-12-16 01:29:26 +0000

    Added Categories to Admin page.

--- final/Store/app/admin/category.rb
+++ final/Store/app/admin/category.rb
@@ -1,6 +1,4 @@
-ActiveAdmin.register Product do
-
-  permit_params :category, :name, :price, :description, :category_id
+ActiveAdmin.register Category do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
